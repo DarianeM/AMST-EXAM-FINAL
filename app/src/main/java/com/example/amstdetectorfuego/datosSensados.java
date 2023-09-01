3e18063b0db4b1d79378b949c6f4eec8b8268ddc
Dariane <dmedrand@espol.edu.ec>
2023-09-01 13:16:22 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/amstdetectorfuego/datosSensados.java
@@ -0,0 +1,16 @@
+package com.example.amstdetectorfuego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class datosSensados extends AppCompatActivity {
+
+    DatabaseReferense db_reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_sensados);
+    }
+}
---
+++ app/src/main/java/com/example/amstdetectorfuego/estadoBateria.java
@@ -0,0 +1,14 @@
+package com.example.amstdetectorfuego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class estadoBateria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_bateria);
+    }
+}
